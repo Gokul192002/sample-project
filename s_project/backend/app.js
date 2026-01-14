6f22094980f1e4d4dc0bd1e49be79c1ad17f3a15
gokul Nath <gokulnathv1919@gmail.com>
2026-01-14 13:23:20 +0530

    add

---
+++ s_project/backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: 'postgres',
+  password: 'postgres',
+  database: 'devops'
+});
+
+app.get('/', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.json({ status: "Backend working", time: result.rows[0] });
+});
+
+app.listen(5000, () => console.log("Backend running on 5000"));
